ca25b9936c1ff86ccc05a9ed8594e62207940f9b
denisaizatullin@gmail.com <denisaizatullin@gmail.com>
2021-11-24 17:10:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static Task2.Stack;
+
+namespace Task2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stackData = new Stack();
+            stackData.Notify += new Message(Request);
+
+            Console.WriteLine("Заполните стек:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                var word = Console.ReadLine();
+                stackData.Push(word);
+            }
+
+            Console.WriteLine("1. Добавить элемент");
+            Console.WriteLine("2. Удалить последний элемент");
+            Console.WriteLine("3. Показать весь стек");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.Write("Выберите действие: ");
+
+                var choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Элемент: ");
+                        string line = Console.ReadLine();
+
+                        stackData.Push(line);
+                        break;
+
+                    case 2:
+                        stackData.Pop();
+                        break;
+
+                    case 3:
+                        var list = stackData.Show();
+                        foreach (var item in list)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("\t\n---- Error ----\n");
+                        break;
+                }
+            }
+        }
+
+        public static void Request(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+    class Stack
+    {
+        private List<string> _stackString = new();
+
+        public delegate void Message(string message);
+
+        public event Message Notify;
+
+        public List<string> Show()
+        {
+            return _stackString;
+        }
+
+        public string Push(string item)
+        {
+            _stackString.Add(item);
+
+            Notify?.Invoke($"Элемент добавлен");
+
+            return item;
+        }
+
+        public string Pop()
+        {
+            if (_stackString == null || _stackString.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                var last = _stackString.LastOrDefault();
+                _stackString.Remove(last);
+
+                Notify?.Invoke($"Элемент {last} удален");
+
+                return last;
+            }
+        }
+    }
+}
+
+
+
+
